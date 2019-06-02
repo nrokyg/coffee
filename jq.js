73ea6e44ddd8988fb391ca71eb46e37fa9ae3684
naru <yugo.0217@gmail.com>
2019-06-02 14:00:43 +0900

    first commit

---
+++ jq.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+$(function() {
+  var h = $(window).height();
+ 
+  $('#wrap').css('display','none');
+  $('#loader-bg ,#loader').height(h).css('display','block');
+});
+ 
+$(window).load(function () { //全ての読み込みが完了したら実行
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+  $('#wrap').css('display', 'block');
+});
